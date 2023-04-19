7a1854a5e08b932f055e745a25087b35eabae2b0
D4vidM0reira <davidsantosms49@gmail.com>
2023-04-19 14:43:34 -0400

    new commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,46 @@
 function verificaEmail() {
-    let email = document.getElementById("email").value;
-    let senha = document.getElementById("senha").value;
+    const email = document.getElementById("email").value;
 
     if (email != "") {
-        if (email.includes("@")) {
-            alert("Endereço de email válido!");
-        } else {
+        if (!email.includes("@")) {
             alert("Endereço de email inválido! Certifique-se de digitar o caractere '@'");
         }
-        if (senha != "") {
-            
-        }
     } else {
-        alert("O preenchimento dos campos é obrigatório!")
+        alert("O preenchimento do campo Email é obrigatório!")
     }
+}
+
+function verificaSenha() {
+    const senha = document.getElementById("senha").value;
+    const caractereEspecial = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    const letraMaiuscula = /[A-Z]/;
+    const numero = /\d/;
+    const msg = ''
+
+    if (senha != "") {
+
+    } else {
+        alert("O preenchimento do campo senha é obrigatório!")
+    }
+}
+
+function verificaSenha() {
+    if (!caractereEspecial.test(senha)) {
+      return false;
+    }
+  
+    if (!letraMaiuscula.test(senha)) {
+      return false;
+    }
+  
+    if (!numero.test(senha)) {
+      return false;
+    }
+    return true;
+}
+
+if (verificaSenha()) {
+console.log("Senha válida!");
+} else {
+console.log("Senha inválida!");
 }
