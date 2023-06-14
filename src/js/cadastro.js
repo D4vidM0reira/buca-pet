8c7cc8b90904779c70f565574d03be2740058f11
D4vidM0reira <davidsantosms49@gmail.com>
2023-06-14 17:45:43 -0400

    ultimas alterações antes da apresentação doprojeto

--- src/js/cadastro.js
+++ src/js/cadastro.js
@@ -6,33 +6,14 @@ function verificaCampos() {
     var eValido = validaFormulario(nome, email, senha1, senha2);
     
     if(eValido){
-        var usuario = salvarUsuario(email, senha1)
-        if (usuario) 
-            window.location.assign("http://127.0.0.1:5500/index.html")
+        if (verificaEmail(email)) {
+            if (verificaSenha(senha1)) {
+                var usuario = salvarUsuario(email, senha1)
+                if (usuario) 
+                    window.location.assign("http://127.0.0.1:5500/index.html")
+            }
+        }
     }
-    // if (nome !== "") {
-    //   if (email !== "") {
-    //     if (verificaEmail(email)) {
-    //       if (senha1 !== "") {
-    //         if (senha2 !== "") {
-    //           verificaSenha();
-    //           var usuario = salvarUsuario(email, senha1)
-    //             if (usuario) 
-    //                 window.location.assign("http://127.0.0.1:5500/index.html")
-
-    //         } else {
-    //           alert('O preenchimento do campo de confirmação de senha é obrigatório!');
-    //         }
-    //       } else {
-    //         alert('O preenchimento do campo de senha é obrigatório!');
-    //       }
-    //     }
-    //   } else {
-    //     alert('O preenchimento do campo de email é obrigatório!');
-    //   }
-    // } else {
-    //   alert('O preenchimento do campo de nome é obrigatório!');
-    // }    
 }
 
 function verificaEmail(email) {
@@ -43,49 +24,42 @@ function verificaEmail(email) {
         return false
     }
 }
+
 function validaFormulario(nome, email, senha1, senha2){
     var msg = [];
-    if(!nome)msg.push("Nome é obrigatório");
-    if(!email)msg.push("Email é obrigatório");
-    if(!senha1)msg.push("Senha  é obrigatório");
+    if(!nome)msg.push("- O preenchimento do campo de nome é obrigatório!");
+    if(!email)msg.push("- O preenchimento do campo de email é obrigatório!");
+    if(!senha1)msg.push("- O preenchimento do campo de senha é obrigatório!");
+    if(!senha2)msg.push("- O preenchimento do campo de confirmação de senha é obrigatório!");
     if(senha1 != senha2)msg.push("Ops! Parece que as senhas não coincidem. Por favor, verifique novamente.")
 
     if(msg.length>0)
         alert(msg.join("\n"));
     
     return msg.length == 0;
-
 }
-function verificaSenha() {
-    const senha1 = document.getElementById("senha1").value;
-    const senha2 = document.getElementById("senha2").value;
-    let msg = '';
-    if (senha1 == senha2) {
-        let temLetraMaiuscula = /[A-Z]/.test(senha1); 
-        let temNumero = /\d/.test(senha1);
-        let temCaractereEspecial = /[!@#$%^&*()\-=_+{}[\]:;'"|,.<>/?]/.test(senha1); 
-        if (temLetraMaiuscula && temNumero && temCaractereEspecial) {
-            alert(`Senha: ${senha1}, válida`)
-        } else {
-            if (!temLetraMaiuscula) {
-                msg += "- A senha deve conter pelo menos uma letra maiúscula \n"
-            }
-            if (!temNumero) {
-                msg += "- A senha deve conter pelo menos um número \n"
-            }
-            if (!temCaractereEspecial) {
-                msg += "- A senha deve conter pelo menos um caracter especial \n"
-            }
-            alert(msg)
-        }
+
+function verificaSenha(senha) {
+    var msg = [];
+    let temLetraMaiuscula = /[A-Z]/.test(senha); 
+    let temNumero = /\d/.test(senha);
+    let temCaractereEspecial = /[!@#$%^&*()\-=_+{}[\]:;'"|,.<>/?]/.test(senha); 
+    if (temLetraMaiuscula && temNumero && temCaractereEspecial) {
+        alert(`Senha: ${senha}, válida`)
+        return true
     } else {
-        alert("Ops! Parece que as senhas não coincidem. Por favor, verifique novamente.")
+        if(!temLetraMaiuscula)msg.push("- A senha deve conter pelo menos uma letra maiúscula")
+        if(!temNumero)msg.push("- A senha deve conter pelo menos um número")
+        if (!temCaractereEspecial)msg.push("- A senha deve conter pelo menos um caracter especial")
+        alert(msg.join("\n"))
+        return false
     }
 }
 
-function salvarUsuario(email, senha) {
+function salvarUsuario(email, senha, nome) {
     if (email && senha) {
         const usuario = {
+            nome: nome,
             email: email,
             senha: senha
         };
--- src/js/login.js
+++ src/js/login.js
@@ -1,23 +1,26 @@
 function acessarSite(email, senha) {
     if (email == 'davidmoreira@gmail.com' && senha == 'insted') {
-        
+        window.location.assign("http://127.0.0.1:5500/index.html")
+    } else {
+        alert('Usuário ou senha inválidos')
     }
 }
 
 function verificaCamposLogin() {
     const email = document.getElementById("email").value;
     const senha = document.getElementById("senha").value;
-    if (email != '') {
-        if (verificaEmail(email)) {
-            if (senha != '') {
-                acessarSite(email, senha)
-            } else {
-                alert('O preenchimento do campo de senha é obrigatório!')
-            }
-        }
-    } else {
-        alert("O preenchimento do campo de email é obrigatório")
-    }
+    (verificaEmail(email)) ? acessarSite(email, senha) : null;
+}
+
+function verificaCampos() {
+    msg = [];
+    if(!email)msg.push("- O preenchimento do campo de email é obrigatório!");
+    if(!senha)msg.push("- O preenchimento do campo de senha é obrigatório!");
+
+    if(msg.length>0)
+    alert(msg.join("\n"));
+
+    return msg.length == 0;
 }
     
 function verificaEmail(email) {
